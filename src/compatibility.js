7083e532163c780c433cad72750978e277237c7d
Charles Miller <shadowmil@gmail.com>
2021-01-24 08:35:09 -0500

    Update compatibility code
    Fixes compatibility issues with tidy5e
    Refactors compatibility out of main.js

--- src/compatibility.js
+++ src/compatibility.js
@@ -1,16 +1,4 @@
-export function injectTidySheet(element, actorId, callback) {
-    let currencyIcon = $(`<a class="currency-control currency-trade" title="Send to Player">
-        <i class="fas fa-balance-scale-right"></i>
-    </a>`)[0];
-    currencyIcon.dataset.actorId = actorId;
-    currencyIcon.addEventListener("click", callback);
-
-    let insertPoint = $("ol.currency li.currency-header", element)[0];
-    let insert = $(`<li class="currency-header flexrow"></li>`).append(currencyIcon);
-    insertPoint.after(insert[0]);
-}
-
-export function injectDnd5e(element, actorId, callback) {
+function currencyDefault(element, actorId, callback) {
     let currencyIcon = $(`<a class="currency-control currency-trade" title="Send to Player">
         <i class="fas fa-balance-scale-right"></i>
     </a>`)[0];
@@ -21,7 +9,19 @@ export function injectDnd5e(element, actorId, callback) {
     insertPoint.after(currencyIcon);
 }
 
-export function injectDndbcs(element, actorId, callback) {
+function currencyTidySheet(element, actorId, callback) {
+    let currencyIcon = $(`<a class="currency-control currency-trade" title="Send to Player">
+        <i class="fas fa-balance-scale-right"></i>
+    </a>`)[0];
+    currencyIcon.dataset.actorId = actorId;
+    currencyIcon.addEventListener("click", callback);
+
+    let insertPoint = $("ul.currency li.currency-header", element)[0];
+    let insert = $(`<li class="currency-header flexrow"></li>`).append(currencyIcon);
+    insertPoint.before(insert[0]);
+}
+
+function currencyDndbcs(element, actorId, callback) {
     let currencyIcon = $(`<a class="currency-control currency-trade" title="Send to Player">
         <i class="fas fa-balance-scale-right"></i>
     </a>`)[0];
@@ -32,7 +32,7 @@ export function injectDndbcs(element, actorId, callback) {
     insertPoint.prepend(currencyIcon);
 }
 
-export function injectCb5es(element, actorId, callback) {
+function currencyCb5es(element, actorId, callback) {
     let currencyIcon = $(`<a class="currency-control currency-trade" title="Send to Player">
         <i class="fas fa-balance-scale-right"></i>
     </a>`)[0];
@@ -43,7 +43,7 @@ export function injectCb5es(element, actorId, callback) {
     insertPoint.append(currencyIcon);
 }
 
-export function injectOgl5e(element, actorId, callback) {
+function currencyOgl5e(element, actorId, callback) {
     let currencyIcon = $(`<a class="currency-control currency-trade" title="Send to Player">
         <i class="fas fa-balance-scale-right"></i>
     </a>`)[0];
@@ -53,3 +53,114 @@ export function injectOgl5e(element, actorId, callback) {
     let insertPoint = $(".inventory-sidebar .currency", element);
     insertPoint.append($(`<div style="text-align: center; padding-top: 10px;"></div>`).append(currencyIcon));
 }
+
+/**
+ * Returns a list item dom elements belonging to the sheet elements
+ * 
+ * @param {object} element The sheet element
+ * 
+ * @returns {object[]}
+ */
+function fetchDefault(element) {
+    let items = $(".inventory.tab .item", element);
+    if (items.length === 0) {
+        items = $(".inventory-list.items-list .item", element);
+    }
+
+    return items;
+}
+
+/**
+ * Injects the trade icon onto a DOM item element.
+ * 
+ * @param {object} item 
+ * @param {function} callback
+ */
+function itemDefault(item, actorId, callback) {
+    const edit = $(".item-control.item-edit", item);
+    const icon = $(`<a class="item-control item-trade" title="Send to Player">
+        <i class="fas fa-balance-scale-right"></i>
+    </a>`)[0];
+
+    icon.dataset.itemId = item.dataset.itemId;
+    icon.dataset.actorId = actorId;
+    icon.addEventListener("click", callback);
+
+    if (edit[0])
+        edit[0].after(icon);
+}
+
+function itemTidy5e(item, actorId, callback) {
+    const edit = $(".item-control.item-edit", item);
+    const icon = $(`<a class="item-control item-trade" title="Send to Player">
+        <i class="fas fa-balance-scale-right"></i>
+        <span class="control-label">Send to Player</span>
+    </a>`)[0];
+
+    icon.dataset.itemId = item.dataset.itemId;
+    icon.dataset.actorId = actorId;
+    icon.addEventListener("click", callback);
+
+    if (edit[0])
+        edit[0].after(icon);
+}
+
+/**
+ * Returns a compatibility name for sheet class
+ * 
+ * @param {string[]} sheetClasses List of classes belonging to the sheet
+ * 
+ * @returns {string}
+ */
+export function sheetCompatibilityName(sheetClasses) {
+    // List of supported sheets
+    const names = [
+        "tidy5e",
+        "alt5e",
+        "dndbcs",
+        "cb5es",
+        "ogl5e-sheet"
+    ];
+
+    for (let i = 0; i < names.length; i++) {
+        if (sheetClasses.includes(names[i])) {
+            return names[i];
+        }
+    }
+
+    // Default Sheet
+    return "defualt";
+}
+
+export const compatibility = {
+    "tidy5e": {
+        currency: currencyTidySheet,
+        fetch: fetchDefault,
+        item: itemTidy5e
+    },
+    "alt5e": {
+        currency: currencyDefault,
+        fetch: fetchDefault,
+        item: itemDefault
+    },
+    "dndbcs": {
+        currency: currencyDndbcs,
+        fetch: fetchDefault,
+        item: itemDefault
+    },
+    "cb5es": {
+        currency: currencyCb5es,
+        fetch: fetchDefault,
+        item: itemDefault
+    },
+    "ogl5e-sheet": {
+        currency: currencyOgl5e,
+        fetch: fetchDefault,
+        item: itemDefault
+    },
+    "defualt": {
+        currency: currencyDefault,
+        fetch: fetchDefault,
+        item: itemDefault
+    },
+};
